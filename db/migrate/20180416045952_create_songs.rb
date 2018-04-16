8852018931eef9ebfc7cc52e0f3369b390db0f00
funo0913 <funo0913@gmail.com>
2018-04-16 14:14:27 +0900

    共通　Songテーブルの列名修正、SeedファイルにAdmin及びGenre、Items以降の要素を追加

--- db/migrate/20180416045952_create_songs.rb
+++ db/migrate/20180416045952_create_songs.rb
@@ -1,9 +1,8 @@
 class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
-      t.string :song_title,    null: false
-
-      t.references :song_item, foreign_key: true
+      t.string :song_title
+      t.integer :cd_item_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Genre.create(:genre_name => 'J-Pop')
+Genre.create(:genre_name => 'K-Pop')
+Genre.create(:genre_name => 'R&B/ソウル')
+Genre.create(:genre_name => 'ダンス')
+Genre.create(:genre_name => 'Jazz')
+Genre.create(:genre_name => 'Rock')
+Genre.create(:genre_name => 'クラシック')
+
+Admin.create(:email => 'test@test.com', :password => 'password')
+Item.create(:artist => 'testArtist', :title_name => 'testAlbam',:label => 'testLabel',:price => 1980,:stock => 10,:image_id => 'test.jpg',:admin_id => 1,:genre_id =>1)
+CdItem.create(:item_id =>1,:cd_title => 'testCDtitle')
+Song.create(:song_title => 'testSong1',:cd_item_id => 1)
+Song.create(:song_title => 'testSong2',:cd_item_id => 1)
