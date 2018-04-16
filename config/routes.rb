b1999db4bd52ad4845e450005e932b899d673cd1
r3250 <n0325.0329@gmail.com>
2018-04-16 12:55:25 +0000

    subadderesses_controller追加,新規投稿画面表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
@@ -14,5 +15,6 @@ devise_for :users, controllers: {
 
  resources :users, only:[:show]
  resources :items, only:[:index,:show]
+ resources :sub_addresses, only:[:index, :new, :edit, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
