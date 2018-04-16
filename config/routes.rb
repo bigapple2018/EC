c342e752344092f69a9325b242a81f4a879ca5dd
r3250 <n0325.0329@gmail.com>
2018-04-16 08:07:02 +0000

    マイページ内容表示のみ実装。controller内編集に必要なアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ devise_for :users, controllers: {
   passwords:     'users/passwords',
   registrations: 'users/registrations'
 }
+
+ resources :users, only:[:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
