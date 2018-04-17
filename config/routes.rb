af55bae8a8a0a43435aaaca4e30a1ed514df9033
r3250 <n0325.0329@gmail.com>
2018-04-17 04:54:23 +0000

    users編集画面修正 userscontroller追加 usermodel変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ devise_for :users, controllers: {
   registrations: 'users/registrations'
 }
 
- resources :users, only:[:show]
+ resources :users, only:[:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
