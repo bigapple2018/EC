49d5430d8cc4cde267bd8f15e1c406336492b83f
funo0913 <funo0913@gmail.com>
2018-04-18 17:57:32 +0900

    商品詳細画面 表示実装

--- app/models/cd_item.rb
+++ app/models/cd_item.rb
@@ -1,2 +1,4 @@
 class CdItem < ApplicationRecord
+  belongs_to :item
+  has_many :song
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :admin
+  belongs_to :genre
+  attachment :image
+  has_many :cd_item
 end
