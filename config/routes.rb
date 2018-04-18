b7fa3c99101e7d7ec20d5a5723a77bb3f28465d1
takashi16 <myfree219@gmail.com>
2018-04-18 10:35:14 +0000

    アドミンのユーザー一覧のコード変更

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 	resources :users
 	resources :items
  end
- # get '/admins_top' => 'admins#top'
+ get '/admins_top' => 'admins#top'
  # get '/admins/users' => 'admins#index'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
