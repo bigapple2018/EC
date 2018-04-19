61d07a70f5a0cbf8f538008ec3f99f4e6505171a
r3250 <n0325.0329@gmail.com>
2018-04-19 12:59:57 +0000

    ユーザー編集画面途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_action :correct_user, only:[:show, :edit, :update]
+
  def show
  	@user = User.find(params[:id])
  end
@@ -15,6 +17,12 @@ class UsersController < ApplicationController
 
    private
    def user_params
-   	  params.require(:user).parmit(:last_name, :first_name, :last_kana, :first_kana, :postal_code, :address, :tell, :email, :password)
+   	  params.require(:user).permit(:last_name, :first_name, :last_kana, :first_kana, :postal_code, :address, :tell, :email, :password, :delete_date)
+   end
+   def correct_user
+   	user = User.find(params[:id])
+   	if current_user != user
+   		redirect_to root_path
+   	end
    end
 end
