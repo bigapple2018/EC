72309ac98b6b725d7b9b141f3b105c87fd80df94
r3250 <n0325.0329@gmail.com>
2018-04-19 16:48:45 +0000

    user編集画面完了 link先はページが出来てから実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
    def correct_user
    	user = User.find(params[:id])
    	if current_user != user
-   		redirect_to root_path
+   	   redirect_to root_path
    	end
    end
 end
