58ab7c5a31b782b2b117eaa1659a6ddc4417d0bd
funo0913 <funo0913@gmail.com>
2018-04-23 13:28:26 +0900

    商品詳細画面　has_many属性の修正

--- app/models/cd_item.rb
+++ app/models/cd_item.rb
@@ -1,4 +1,4 @@
 class CdItem < ApplicationRecord
   belongs_to :item
-  has_many :song, ->{order("song_number ASC") }
+  has_many :songs, ->{order("song_number ASC") }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
 
   belongs_to :genre
   attachment :image
-  has_many :cd_item
+  has_many :cd_items
 end
