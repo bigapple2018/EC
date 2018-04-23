18acbb47d8781c80a43a935bc3d6320a7762e5eb
funo0913 <funo0913@gmail.com>
2018-04-23 20:27:35 +0900

    商品一覧画面　不要なパラメータ設定を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,4 @@ class ItemsController < ApplicationController
     @q = Item.search(params[:q])
     @items = @q.result(distinct: true)
   end
-  def search_params
-    params.require(:q).permit(:title_name_cont,:genre_id_eq)
-  end
 end
