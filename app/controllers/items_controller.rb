8da10ad1864e9efa29309ffdd0470895b82b323d
funo0913 <funo0913@gmail.com>
2018-04-23 15:24:35 +0900

    商品一覧画面　利用者ヘッダー・検索フォームを別ファイルに反映　検索処理の初期化処理が不足していたため追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    item_search_init
     @genres = Genre.all
+  end
+
+  def show
+    item_search_init
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_search_init
     @q = Item.search(params[:q])
     @items = @q.result(distinct: true)
   end
-
   def search_params
     params.require(:q).permit(:title_name_cont)
-  end
-  def show
-    @item = Item.find(params[:id])
   end
 end
