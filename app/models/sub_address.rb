56b5ce5580ed1f2d9c30af472138ed871749939e
r3250 <n0325.0329@gmail.com>
2018-04-23 11:24:09 +0000

    sub_address画面編集完了

--- app/models/sub_address.rb
+++ app/models/sub_address.rb
@@ -1,7 +1,10 @@
 class SubAddress < ApplicationRecord
 
 belongs_to :user
-validates :sub_postal_code, presence: true
+POSTAL_TELL_REGEXP = /\A[0-9¥-]+\z/
+
 validates :sub_address, presence: true
+validates :sub_postal_code, presence: true
+validates :sub_postal_code, format: { with: POSTAL_TELL_REGEXP, message: "数字とハイフンのみが使用できます" }, length: { minimum: 8 },if:"sub_postal_code.present?"
 
 end
