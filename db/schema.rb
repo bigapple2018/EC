2da6e2b9630e627b4f67968b23c483387d20e935
r3250 <n0325.0329@gmail.com>
2018-04-26 08:34:14 +0000

    validate日本語対応 不要ファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424125238) do
+ActiveRecord::Schema.define(version: 20180426082534) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -71,10 +71,8 @@ ActiveRecord::Schema.define(version: 20180424125238) do
     t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["admin_id"], name: "index_items_on_admin_id"
     t.index ["genre_id"], name: "index_items_on_genre_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "leaves", force: :cascade do |t|
