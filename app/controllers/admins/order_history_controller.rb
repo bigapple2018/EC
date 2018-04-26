5e2e4806636fb136c95e7cd7716e0765bd4cbc4d
funo0913 <funo0913@gmail.com>
2018-04-26 18:53:47 +0900

    管理者サイト　ユーザ購入履歴画面作成

--- app/controllers/admins/order_history_controller.rb
+++ app/controllers/admins/order_history_controller.rb
@@ -1,7 +1,7 @@
 class Admins::OrderHistoryController < ApplicationController
 
 	def index
-		@order_histories = OrderHistories.all
+		@histories = OrderHistory.where(user_id: params[:user_id]).page(params[:page]).per(5)
+		@user = User.find(params[:user_id])
 	end
-
 end
