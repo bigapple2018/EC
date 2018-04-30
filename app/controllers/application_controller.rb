b6aa5c4a03d3f01c2e4c42b916b3d155eba0b2c2
funo0913 <funo0913@gmail.com>
2018-04-30 15:51:55 +0900

    カートの有無判定修正、カートの商品数の表示バッジ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,33 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
  helper_method :current_cart
+ helper_method :current_cart_inside
 
 	def current_cart
 		if session[:cart_id]
 			@cart = Cart.find(session[:cart_id])
-		else
-			@cart = Cart.create
+		elsif Cart.find_by(:user_id => current_user.id)
+      @cart = Cart.find_by(:user_id => current_user.id)
+      session[:cart_id] = @cart.id
+    else
+			@cart = Cart.create(
+        :user_id => current_user.id
+      )
 			session[:cart_id] = @cart.id
 		end
 	end
+
+  def current_item_count
+    if session[:cart_id]
+      cart = Cart.find(session[:cart_id])
+      count = cart.item_carts.count
+    elsif Cart.find_by(:user_id => current_user)
+      cart = Cart.find_by(:user_id => current_user)
+      count = cart.item_carts.count
+    else
+      count = 0
+    end
+      count
+  end
+
 end
